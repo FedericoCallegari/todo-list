5b04b76fe1b37f240aedb2f7e2263ab7d6d673da
Federico Callegari <callegarifede3@gmail.com>
2024-09-26 14:21:49 -0300

    Add functions inside todo.store

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -1,9 +1,28 @@
-import { Todo } from "../todos/models/todo.model";
+import { Todo } from'../todos/models/todo.model';
+
+const Filters ={
+    All: 'all',
+    Completed:'Completed',
+    Pending: 'Pending',
+}
 
 const state = {
     todos: [
         new Todo('Piedra del alma'),
         new Todo('Piedra del infinito'),
         new Todo('Piedra de marfil')
-    ]
+    ],
+    filter:Filters.All
+    
+}
+
+const initStore = ()=>{
+    console.log(state);
+    
+    console.log('initStore');
+    
+}
+
+export default {
+    initStore
 }
